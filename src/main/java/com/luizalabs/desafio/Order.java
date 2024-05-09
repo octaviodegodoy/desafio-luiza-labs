97270317bfcb2dc5ec669e1f9194899a3dd77b5b
octaviodegodoy <octaviodegodoy@gmail.com>
2024-05-09 15:40:04 -0300

    Creating objects

---
+++ src/main/java/com/luizalabs/desafio/Order.java
@@ -0,0 +1,11 @@
+package com.luizalabs.desafio;
+
+import java.util.List;
+
+public class Order {
+    private int order_id;
+    private String total;
+    private String date;
+    private List<Product> products;
+
+}
